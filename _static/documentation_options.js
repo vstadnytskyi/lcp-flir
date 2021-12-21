fab83a5bc2332af1f984da7625c8e74da4057e8b
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2021-12-21 17:02:31 +0000

    Update docs after building Travis build 20 of vstadnytskyi/lcp-flir
    The docs were built from the branch 'master' against the commit
    260434b1bf28e53b1943a3c3132b76ff27557aff.
    
    The Travis build that generated this commit is at
    https://app.travis-ci.com/vstadnytskyi/lcp-flir/jobs/553122418.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.7.1/bin/doctr deploy . --built-docs docs/build/html/

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post21+g91755c6',
+    VERSION: '0.post24+g260434b',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
