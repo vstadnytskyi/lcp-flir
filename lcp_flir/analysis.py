8b31620b886aa6cc5a084df87abd0e7caada2a54
Valentyn Stadnytskyi <v.stadnytskyi@gmail.com>
2022-05-13 09:55:50 -0400

    analysis

--- lcp_flir/analysis.py
+++ lcp_flir/analysis.py
@@ -5,3 +5,39 @@ TODO:
 - Move function that convert bytes stream to images with given pixel format
     - Supported Pixel Format: Mono12p, Mono16, Mono8, BayerRG12p, BayerRG16
 """
+
+def convert_raw_mono12p_to_image(data, shape = None):
+    """
+    converts bytes stream of 12bit numbers to a 2D array representing an image
+
+    see http://softwareservices.flir.com/BFS-U3-89S6/latest/Model/public/ImageFormatControl.html for details on how to parse the bytes stream.
+
+    Parameters
+    ----------
+    data : 1D array
+    shape : tuple
+
+    Returns
+    -------
+    array
+        2D array with given height(number of rows) and width(number of colums).
+    """
+    pass
+
+def convert_raw_mono16_to_image(data, shape = None):
+    """
+    converts bytes stream of 16bit numbers to a 2D array representing an image
+
+    see http://softwareservices.flir.com/BFS-U3-89S6/latest/Model/public/ImageFormatControl.html for details on how to parse the bytes stream.
+
+    Parameters
+    ----------
+    data : 1D array
+    shape : tuple
+
+    Returns
+    -------
+    array
+        2D array with given height(number of rows) and width(number of colums).
+    """
+    pass
